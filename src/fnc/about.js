edaf2bbaff7ddbf02eefc042d1a8e3b9da56b8b8
ysys <1243933010@qq.com>
2020-12-08 18:37:50 +0800

    ylrc

--- src/fnc/about.js
+++ src/fnc/about.js
@@ -1,3 +1,33 @@
 
+import router from '@/router/index'
+import store from '@/store/index'
 
 
+export function timeSetVuex(str) {
+  store.dispatch('timeOut', str)
+}
+
+export function setVuex() {
+  store.commit('setVuexA', '被修改后的内容');
+}
+
+
+export function addRoute() {
+  const arr = [
+    {
+      path: "/test",
+      name: "Test",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "@/views/Test.vue"),
+    },
+    {
+      path: "/test2",
+      name: "Test2",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "@/views/Test2.vue"),
+    },
+  ];
+  arr.forEach((val) => {
+    router.addRoute(val);
+  });
+}
--- src/views/About.vue
+++ src/views/About.vue
@@ -5,20 +5,20 @@
     <aboutComponent :msg="msg" ref="aboutRef" />
     <div>**********************</div>
     <input type="text" v-model="name" />
+    <span>{{ nameLen }}</span>
     <div @click="addRoute">添加一个路由</div>
-    <div>{{vuexA}}</div>
+    <div>{{ vuexA }}</div>
     <div @click="setVuex">修改vuex的内容</div>
-    <div @click="timeSetVuex">异步修改vuex的内容</div>
+    <div @click="timeSetVuex(name)">异步修改vuex的内容</div>
     <div>------------------------------------------</div>
-      <input type="text" placeholder="输入username" v-model="username">
-      <input type="text" placeholder="输入age" v-model="age">
-      <el-button type="success" @click="add">确定添加</el-button>
+    <input type="text" placeholder="输入username" v-model="username" />
+    <input type="text" placeholder="输入age" v-model="age" />
+    <el-button type="success" @click="add">确定添加</el-button>
     <div>------------------------------------------</div>
     <div>列表</div>
-    <div v-for="(item,index) in arr" :key="index">
-      <p>{{item.username}}</p>
-      <p>{{item.age}}</p>
-
+    <div v-for="(item, index) in arr" :key="index">
+      <p>{{ item.username }}</p>
+      <p>{{ item.age }}</p>
     </div>
     <!-- <div>{{name}}</div> -->
   </div>
@@ -26,76 +26,53 @@
 
 
 <script lang="ts">
-//  function add(arr,username,age){
-//       const obj = {username,age};
-//       arr.push(obj);
-//   }
-
 import aboutComponent from "@/components/AboutComponent.vue";
-import { onMounted ,ref,provide} from "vue";
+import { onMounted, ref, provide, computed, reactive } from "vue";
 import router from "../router/index";
 import store from "../store/index";
-import { mapState } from 'vuex';
-import { reactive } from 'vue';
+import { mapState } from "vuex";
+import { addRoute, timeSetVuex, setVuex } from "../fnc/about.js";
 export default {
   components: {
     aboutComponent,
   },
   setup(props, content) {
-    const msg = ref("prop msg1");
-    const name = ref("yue");
+    const msg = ref<string>("prop msg1");
+    const name = ref<string>("yue");
+    const nameLen = computed(() => name.value.length);
 
-console.log(content)
     const arr = reactive([]);
-    const username = ref('yue');
-    const age = ref('24');
-    function add(){
-      const obj = {username:username.value,age:age.value};
+    const username = ref("yue");
+    const age = ref("24");
+    function add() {
+      const obj = { username: username.value, age: age.value };
       arr.push(obj);
     }
-  
     provide("provideData", "123456");
     onMounted(() => {
       console.log("onMounted");
-      // console.log(vuexDate.vuexA);
+      console.log(addRoute);
     });
-    return { msg, name ,arr,username,age,add};
+
+    return {
+      msg,
+      name,
+      nameLen,
+      arr,
+      username,
+      age,
+      add,
+      addRoute,
+      setVuex,
+      timeSetVuex,
+    };
   },
-  computed: mapState(['vuexA']),
- 
+  computed: mapState(["vuexA"]),
   methods: {
-    
     getChildrenMsg() {
       console.log(this.$refs.aboutRef);
     }, //获取子组件信息
-    addRoute() {
-      const arr = [
-        {
-          path: "/test",
-          name: "Test",
-          component: () =>
-            import(/* webpackChunkName: "about" */ "@/views/Test.vue"),
-        },
-        {
-          path: "/test2",
-          name: "Test2",
-          component: () =>
-            import(/* webpackChunkName: "about" */ "@/views/Test2.vue"),
-        },
-      ];
-      arr.forEach((val) => {
-        router.addRoute(val);
-      });
-    }, //addrouter
-    setVuex() {
-      const $store: any = store;
-      $store.commit('setVuexA','被修改后的内容');
-      console.log($store)
-    },
-    timeSetVuex(){
-      const $store: any = store;
-      $store.dispatch('timeOut','异步修改后的值')
-    }
+    //addrouter
   },
 };
 </script>
